a9c22d81658ad73541a56299e67b91b0863231f6
Jungmin-YUN-0 <76892989+Jungmin-YUN-0@users.noreply.github.com>
2022-09-06 20:22:21 +0900

    Update bert_stsa_lower.sh

--- src/scripts/bert_stsa_lower.sh
+++ src/scripts/bert_stsa_lower.sh
@@ -1,54 +1,18 @@
 #!/usr/bin/env bash
 
-SRC=~/readability/text_augmentation/TransformersDataAugmentation/src
-CACHE=~/CACHE
+SRC=/home/jungmin/readability/text_augmentation/TransformersDataAugmentation/src
+CACHE=/home/jungmin/readability/text_augmentation/TransformersDataAugmentation/CACHE
+
 TASK=stsa
 
 for NUMEXAMPLES in 10;
 do
     for i in {0..14};
         do
-        RAWDATADIR=~/readability/text_augmentation/TransformersDataAugmentation/src/utils/datasets/${TASK}/exp_${i}_${NUMEXAMPLES}
+        RAWDATADIR=$SRC/utils/datasets/${TASK}/exp_${i}_${NUMEXAMPLES}
 
        # Baseline classifier
-        python $SRC/bert_aug/bert_classifier.py --task $TASK  --data_dir $RAWDATADIR --seed ${i} --learning_rate $BERTLR --cache $CACHE > $RAWDATADIR/bert_baseline.log
-
-      ##############
-      ## EDA
-      ##############
-
-      EDADIR=$RAWDATADIR/eda
-      mkdir $EDADIR
-      python $SRC/bert_aug/eda.py --input $RAWDATADIR/train.tsv --output $EDADIR/eda_aug.tsv --num_aug=1 --alpha=0.1 --seed ${i}
-      cat $RAWDATADIR/train.tsv $EDADIR/eda_aug.tsv > $EDADIR/train.tsv
-      cp $RAWDATADIR/test.tsv $EDADIR/test.tsv
-      cp $RAWDATADIR/dev.tsv $EDADIR/dev.tsv
-      python $SRC/bert_aug/bert_classifier.py --task $TASK --data_dir $EDADIR --seed ${i} --learning_rate $BERTLR --cache $CACHE  > $RAWDATADIR/bert_eda.log
-
-
-        #######################
-        # GPT2 Classifier
-        #######################
-
-        GPT2DIR=$RAWDATADIR/gpt2
-        mkdir $GPT2DIR
-        python $SRC/bert_aug/cgpt2.py --data_dir $RAWDATADIR --output_dir $GPT2DIR --task_name $TASK  --num_train_epochs 25 --seed ${i} --top_p 0.9 --temp 1.0 --cache $CACHE
-        cat $RAWDATADIR/train.tsv $GPT2DIR/cmodgpt2_aug_3.tsv > $GPT2DIR/train.tsv
-        cp $RAWDATADIR/test.tsv $GPT2DIR/test.tsv
-        cp $RAWDATADIR/dev.tsv $GPT2DIR/dev.tsv
-        python $SRC/bert_aug/bert_classifier.py --task $TASK --data_dir $GPT2DIR --seed ${i} --cache $CACHE > $RAWDATADIR/bert_gpt2_3.log
-
-    #    #######################
-    #    # Backtranslation DA Classifier
-    #    #######################
-
-    BTDIR=$RAWDATADIR/bt
-    mkdir $BTDIR
-    python $SRC/bert_aug/backtranslation.py --data_dir $RAWDATADIR --output_dir $BTDIR --task_name $TASK  --seed ${i} --cache $CACHE
-    cat $RAWDATADIR/train.tsv $BTDIR/bt_aug.tsv > $BTDIR/train.tsv
-    cp $RAWDATADIR/test.tsv $BTDIR/test.tsv
-    cp $RAWDATADIR/dev.tsv $BTDIR/dev.tsv
-    python $SRC/bert_aug/bert_classifier.py --task $TASK --data_dir $BTDIR --seed ${i} --cache $CACHE  > $RAWDATADIR/bert_bt.log
+        python $SRC/bert_aug/bert_classifier.py --task $TASK  --data_dir $RAWDATADIR --seed ${i} --cache $CACHE > $RAWDATADIR/bert_baseline.log
 
    # #######################
    # # CBERT Classifier
